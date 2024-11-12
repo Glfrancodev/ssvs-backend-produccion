f35ac60538ab85dd4c6b844f69361107d553a746
Glfrancodev <gabrielandreslopezfrancodev@gmail.com>
2024-11-12 08:03:00 -0400

    deploy 5

--- src/main/java/com/ssvs/seguro_salud_vida_sana/controllers/AseguradoController.java
+++ src/main/java/com/ssvs/seguro_salud_vida_sana/controllers/AseguradoController.java
@@ -20,6 +20,7 @@ import com.ssvs.seguro_salud_vida_sana.services.AseguradoService;
 
 @RestController
 @RequestMapping("/api/asegurado")
+@CrossOrigin(origins = {"http://192.168.1.9", "https://ssvs-frontend-produccion-production.up.railway.app/"})
 public class AseguradoController {
 
     @Autowired
